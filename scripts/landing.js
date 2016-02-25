2baa1b40da1f7bcd433370cbea1305d78df54fb9
R.S. Richards <RobertoRichards2@Robertos-MacBook-Pro.local>
2016-02-25 17:51:01 -0500

    Fixed points animation

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,31 +1,31 @@
-var animatePoints = function() {
+//var animatePoints = function() {
 
 var revealPoint = function() {
   $(this).css({
     opacity: 1,
     transform: 'scaleX(1) translateY(0)'
-});
+  });
 };
 
-$.each($('.point'), revealPoint);
-};
+//$.each($('.point'), revealPoint);
+//};
 
-var animatePoints = function(points) {
-  forEach(points, revealPoint);
+var animatePoints = function() {
+   $.each($('.point'), revealPoint);  //forEach(points, revealPoint);
 };
 
 $(window).load(function() {
 
-if ($(window).height() > 950) {
-  animatePoints();
-}
-
-var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
-
-$(window).scroll(function(event) {
-  if ($(window).scrollTop() >= scrollDistance) {
+  if ($(window).height() > 950) {
     animatePoints();
-
   }
+
+  var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+
+  $(window).scroll(function(event) {
+    if ($(window).scrollTop() >= scrollDistance) {
+      animatePoints();
+    }
   });
+
 });
